bbfa2db35b705684f43f72f2486a04fa6032f7df
younfor <younfor@yeah.net>
2015-02-25 16:23:01 +0800

    在线排序和统计

--- gui/guiMainQQ.py
+++ gui/guiMainQQ.py
@@ -107,8 +107,8 @@ class Ui_Main(object):
             scene = QtGui.QGraphicsScene(main)
             item = QtGui.QGraphicsPixmapItem(pixmap)
             scene.addItem(item)
-            self.graphicsView[i].setScene(scene)
-            self.graphicsView[i].resize(50,50)
+            self.graphicsView[data['friends'][i]['uin']].setScene(scene)
+            self.graphicsView[data['friends'][i]['uin']].resize(50,50)
 
     def setupFriend(self, data,online):
         # categories
@@ -124,59 +124,75 @@ class Ui_Main(object):
                 self.listWidget[cat['index']], _fromUtf8(cat['name']))
             self.listWidget[cat['index']].setGeometry(
                 QtCore.QRect(0, 1, 238, 301))
-        # markname  
-        self.uindict={}
-        for mark in data['marknames']:
-            info=[mark['markname'],None,None]
-            self.uindict[mark['uin']]=info
-        # online
-        for on in online:
-            u=self.uindict.get(on['uin'])
-            if u is None:
-                info=[None,True,None]
-                self.uindict[on['uin']]=info
-            else:
-                u[1]=True
-        # widget
-        loc = -1
         self.graphicsView = {}
-        for user in data['friends']:
-            loc += 1
-            if self.uindict.get(user['uin']):
-                markname=self.uindict.get(user['uin'])[0]
+        # userlist
+        self.userdict={}
+        for i in range(len(data['friends'])):
+            info={'markname':'None','nickname':data['info'][i]['nick'],'online':False,'cat':data['friends'][i]['categories']}
+            print info
+            self.userdict[data['friends'][i]['uin']]=info
+        # markname   
+        for mark in data['marknames']:
+            self.userdict[mark['uin']]['markname']=mark['markname']
+        # online
+        cat_count={}
+        for on in online:
+            self.userdict.get(on['uin'])['online']=True
+            arg1=self.listWidget[self.userdict[on['uin']].get('cat')]
+            arg2=self.userdict[on['uin']].get('nickname')
+            arg3=self.userdict[on['uin']].get('markname')
+            arg4=on['uin']
+            self.item, self.widget = self.createWidget(arg1,arg2,arg3,arg4)
+            arg1.setItemWidget(
+                self.item, self.widget)
+            #count
+            if cat_count.get(self.userdict[on['uin']].get('cat')):
+                print True
+                cat_count[self.userdict[on['uin']].get('cat')]+=1
             else:
-                markname=None
-            self.item, self.widget = self.createWidget(
-                loc, self.listWidget[user['categories']], data['info'][loc]['nick'],markname,user['uin'])
-            self.listWidget[user['categories']].setItemWidget(
+                cat_count[self.userdict[on['uin']].get('cat')]=1
+            print 'cat:',self.userdict[on['uin']].get('cat'),'value:',cat_count[self.userdict[on['uin']].get('cat')]
+        # widget
+        for user in data['friends']:
+            if self.userdict[user['uin']].get('online')==True:
+                continue
+            arg1=self.listWidget[self.userdict[user['uin']].get('cat')]
+            arg2=self.userdict[user['uin']].get('nickname')
+            arg3=self.userdict[user['uin']].get('markname')
+            arg4=user['uin']
+            self.item, self.widget = self.createWidget(arg1,arg2,arg3,arg4)
+            arg1.setItemWidget(
                 self.item, self.widget)
         # size
         self.toolsize = []
+        j=-1
         for key,val in self.listWidget.items():
+            j+=1
             size = len(val)
             print size
             self.toolsize.append(size)
+            self.toolBox.setItemText(j,self.toolBox.itemText(j)+'('+str(cat_count.get(key,0))+'/'+str(size)+')')
         self.toolBox.currentChanged.connect(self.onToolBoxChanged)
         self.toolBox.resize(QtCore.QSize(250,self.toolsize[0]*48+len(self.toolsize)*34))
     def onToolBoxChanged(self,index):
         self.toolBox.resize(QtCore.QSize(250,self.toolsize[index]*48+len(self.toolsize)*34))
-    def createWidget(self, loc, listWidget, title,markname,uin):
+    def createWidget(self,listWidget, title,markname,uin):
         self.listWidgetItem = QtGui.QListWidgetItem(listWidget)
         self.listWidgetItem.setSizeHint(QtCore.QSize(0, 48))
         self.widget = QtGui.QWidget()
         self.widget.setGeometry(QtCore.QRect(0, 0, 238, 51))
-        self.graphicsView[loc] = QtGui.QGraphicsView(self.widget)
-        self.graphicsView[loc].setGeometry(QtCore.QRect(1, 1, 38, 38))
+        self.graphicsView[uin] = QtGui.QGraphicsView(self.widget)
+        self.graphicsView[uin].setGeometry(QtCore.QRect(1, 1, 38, 38))
         self.lbl_title = QtGui.QLabel(self.widget)
         self.lbl_title.setGeometry(QtCore.QRect(60, 10, 181, 18))
         self.lbl_title.setFont(self.font2)
-        if markname is not None:
+        if markname != 'None':
             title=markname+'('+title+')'
         self.lbl_title.setText(_translate("Main", title, None))
         self.lbl_comment = QtGui.QLabel(self.widget)
         self.lbl_comment.setGeometry(QtCore.QRect(60, 30, 181, 18))
-        info=self.uindict.get(uin)
-        if info and info[1]:
+        info=self.userdict.get(uin)
+        if info['online']:
             self.lbl_comment.setText(_translate("Main", '[在线]', None))
         else:
             self.lbl_comment.setText(_translate("Main", '[离线]', None))
